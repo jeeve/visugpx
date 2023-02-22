9cd3bf991b2dfef049a475873b772d6f34feacb1
jeeve <jyvaisj@gmail.com>
2023-02-22 23:19:29 +0100

    multipart

--- src/app/upload.service.ts
+++ src/app/upload.service.ts
@@ -1,18 +1,21 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
- 
+
 @Injectable({
- providedIn: 'root'
+  providedIn: 'root',
 })
 export class UploadService {
- 
- constructor(
-   private httpClient: HttpClient,
- ) { }
- 
- public uploadfile(file: File) {
-   let formParams = new FormData();
-   formParams.append('file', file)
-   return this.httpClient.post('fileupload.php', formParams)
- }
+  constructor(private httpClient: HttpClient) {}
+
+  public uploadfile(file: File) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'multipart/form-data',
+      }),
+    };
+
+    let formParams = new FormData();
+    formParams.append('file', file);
+    return this.httpClient.post('fileupload.php', formParams, httpOptions);
+  }
 }
